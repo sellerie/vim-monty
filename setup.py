5677654631a34ab21c7de4706c911d29e0e6c78e
Maik Brendler <sellerieschnitzel@googlemail.com>
2012-03-31 09:28:43 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="vim_python",
+    version="0.0.1",
+    author="Maik Brendler",
+    author_email="sellerieschnitzel@googlemail.com",
+    description="",
+    long_description="",
+    license="BSD",
+    keywords="",
+    url="",
+    packages=['vim_python'],
+    classifiers=[],
+)

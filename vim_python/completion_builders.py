35738002dea9b0890640b9d2133924c1234b6339
Maik Brendler <sellerieschnitzel@googlemail.com>
2012-04-17 10:39:15 +0200

    Add missing module 'completion_builders'.

---
+++ vim_python/completion_builders.py
@@ -0,0 +1,25 @@
+"""Should contain functions to build a completion entry by one language element
+
+At this point only a builder for VIM exists.
+"""
+from vim_python.logger import log
+
+
+def vim_completion_builder(completionable):
+    """Completion builder for a entry of the VIM omni completion.
+    """
+    try:
+        menu = '' # completionable.filename() + ':' + completionable.linenumber()
+        word = completionable.complex_name()
+        return {
+            'word': word,
+            'abbr': word,
+            'kind': completionable.kind(),
+            'menu': menu,
+            'dup': '1',
+        }
+    except Exception, exc:
+        log(exc)
+        import traceback
+        log(traceback.format_exc())
+        return completionable.name()
